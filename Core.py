82fb7f16d692e976418f21e1547d6f15e8384dbe
QMQMT1 <mn77011(0@gmail.com>
2025-07-27 00:31:50 +0300

    Add icon

--- Core.py
+++ Core.py
@@ -1,25 +1,24 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((800, 600))
-clock = pygame.time.Clock()
-running = True
 
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('Live solo')
+
+
+try:
+    icon = pygame.image.load('image/assets_task_01k1476qdmfjsr95yxtwsdf192_1753562211_img_1.JPG')
+    pygame.display.set_icon(icon)
+except:
+    print("Ошибка загрузки иконки! Проверьте путь к файлу")
+
+running = True
 while running:
+    screen.fill((66,135,245))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-        screen.fill('red')
-
-        # Define polygon points (a triangle)
-        polygon_points = [(100, 300), (300, 100), (500, 300)]
-
-        # Draw a filled red polygon
-        pygame.draw.polygon(screen, (0, 255, 0), polygon_points)
-
-        pygame.display.flip()
-
-        clock.tick(60)
+    pygame.display.update()
 
 pygame.quit()
