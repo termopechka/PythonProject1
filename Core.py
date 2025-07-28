04beed22f1f1ea3c7f56d03a4fddf60722b72ef1
QMQMT1 <mn77011(0@gmail.com>
2025-07-28 03:05:21 +0300

    Add wasd and png

--- Core.py
+++ Core.py
@@ -2,38 +2,42 @@ import pygame
 
 pygame.init()
 
-screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption('Live solo')
-
-
-try:
-    icon = pygame.image.load('image/assets_task_01k1476qdmfjsr95yxtwsdf192_1753562211_img_1.JPG')
-    pygame.display.set_icon(icon)
-except:
-    print("Ошибка загрузки иконки! Проверьте путь к файлу")
 
 running = True
-
-screen = pygame.display.set_mode((700, 400))
+h = 1200
+w = 800
+screen = pygame.display.set_mode((h,w))
 pygame.display.set_caption('Live solo')
 
 
-try:
-    icon = pygame.image.load('image/assets_task_01k1476qdmfjsr95yxtwsdf192_1753562211_img_1.JPG')
-    pygame.display.set_icon(icon)
-except:
-    print("Ошибка загрузки иконки! Проверьте путь к файлу")
 
+icon = pygame.image.load('image/assets_task_01k1476qdmfjsr95yxtwsdf192_1753562211_img_1.JPG')
+pygame.display.set_icon(icon)
+hero = pygame.image.load('image/hero_img/Layer 1_sprite_1.png')
+hero = pygame.transform.scale(hero, (70, 70))
+hero.set_colorkey((255, 255, 255))
+x = h // 2
+y = w // 2
 while running:
 
-    screen.fill((66,135,245))
+    screen.fill((100, 0, 0))
+    screen.blit(hero, (x, y))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                x -= 50
+            elif event.key == pygame.K_d:
+                x += 50
+            elif event.key == pygame.K_w:
+                y -= 50
+            elif event.key == pygame.K_s:
+                y += 50
 
-
-
-    pygame.display.update()
+            elif event.key == pygame.K_ESCAPE:
+                running = False
+    pygame.display.flip()
 
 pygame.quit()
