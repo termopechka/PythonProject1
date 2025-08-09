be648383600f35828009a11689a31f667650cd8c
termopechka <kanditalex@gmail.com>
2025-08-10 00:55:53 +0300

    Added part code about NPCs and attribute

--- NPC.py
+++ NPC.py
@@ -27,10 +27,26 @@ class NPC(pygame.sprite.Sprite):
         self.y = y
         self.radius = 10
         self.isdialogactive = False
-        self.icon_interaction = pygame.transform.scale(pygame.image.load('assets/icon.png'), (50, 50))
-    def detect_player(self, x, y):
+        self.icon_interaction = pygame.transform.scale(pygame.image.load('image/images.jpg'), (50, 50))
+        self.list = []
+        for i in range(0,4):
+            img = pygame.image.load(f'image/images.jpg')
+            img = pygame.transform.scale(img, (150, 150))
+            self.list.append(img)
+        self.sprites_now = 0
+        self.speed_sprites = 0.15
+    def detect_player(self, x, y, screen):
         self.distance = math.sqrt((self.x - x) ** 2 + (self.y - y) ** 2)
         if self.distance <= self.radius:
+            screen.blit(self.icon_interaction, (50, 50))
 
-    def draw_npc(self):
+    def update(self):
+        self.sprites_now += self.speed_sprites
+        self.sprites_now %= len(self.list)
+        self.image = self.list[int(self.sprites_now)]
+        self.image.set_colorkey((255, 255, 255))
+        self.rect = self.image.get_rect(center=(0,0))
+
+    def draw_npc(self, screen):
+        screen.blit(self.image, (50, 50))
 
