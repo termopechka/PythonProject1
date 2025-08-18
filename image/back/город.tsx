dbc81dc0e433caf3af03e06df1298cee68077b82
termopechka <kanditalex@gmail.com>
2025-08-18 23:05:49 +0300

    Add collision objects, remake background, map

---
+++ image/back/город.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="город" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="город.png" width="512" height="512"/>
+</tileset>
